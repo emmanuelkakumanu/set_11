3551360e4012a458ede45ae72ebe183e3ed7a55b
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-14 09:44:09 +0530

    Update 105.java

--- 105.java
+++ 105.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -13,11 +11,7 @@ class Ideone
 		int c=a;
 		int d[]=new int[10];
 		int i=0,cnt=0;
-		if(b==0)
-		{
-			System.out.print(a+""+b);
-		}
-		else if(a==0)
+		if(a==0 || b==0)
 		{
 			System.out.print(a+""+b);
 		}
