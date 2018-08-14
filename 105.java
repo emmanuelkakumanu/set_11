8fa59e60dbc074782c49754e300279a1ef41216a
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-14 09:42:15 +0530

    join two int numbers

---
+++ 105.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a;
+		int d[]=new int[10];
+		int i=0,cnt=0;
+		if(b==0)
+		{
+			System.out.print(a+""+b);
+		}
+		else if(a==0)
+		{
+			System.out.print(a+""+b);
+		}
+		else
+		{
+		while(b!=0)
+		{
+			int rem=b%10;
+			d[i++]=rem;
+			cnt++;
+			b=b/10;
+		}
+		for(int j=cnt-1;j>=0;j--)
+		{
+			c=(c*10)+d[j];
+		}
+		System.out.print(c);
+		}
+	}
+}
